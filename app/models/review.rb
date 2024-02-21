a97a51ba505a14aad3cbcd6af7195f3b0e9be506
Raj Shrivansh <rajshrivansh3@gmail.com>
2024-02-21 15:38:15 +0530

    Final commit last

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,4 @@
 class Review < ApplicationRecord
     belongs_to :play
     belongs_to :user
-    validates :comment, presence: true, length: {minimum:50}
-    validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5 }
 end
